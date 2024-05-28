23d95d227d6ffab99e3807d26d26a83f0100f1b0
ShaikhMFazaluddin <fazaluddin2806@gmail.com>
2024-05-28 12:53:45 +0530

    bubble sort working now

--- app/ui/canvas.tsx
+++ app/ui/canvas.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { useDraw } from "./useDraw";
-import { sort } from "../lib/sort";
 
 export default function Canvas() {
 
@@ -37,11 +36,20 @@ export default function Canvas() {
     }
 
     function initGraph() {
+
+        let initDraw = new CustomEvent("changeDraw", {
+            detail: {
+                px: 0,
+                height: 0,
+            }
+        });
         for (let i = 0; i < max; i++) {
             arr.push(Math.round(Math.random() * maxY));
+            initDraw.detail.px = i + 10;
+            initDraw.detail.height = arr[i];
+            canvasRef.current?.dispatchEvent(initDraw);
         }
     }
-
     initGraph();
 
     const reset = async () => {
@@ -62,10 +70,66 @@ export default function Canvas() {
         }
     }
 
+    async function bubbleSort(arr: number[]): void {
+        const foreColor = "white", backColor = "black", activeColor = 'red';
+        let i: number, j, temp: number;
+        let drawEvent = new CustomEvent("changeDraw", {
+            detail: {
+                px: 0,
+                height: 0,
+                color: foreColor 
+            }
+        });
+
+        for (j = 1; j < arr.length; j++) {
+            if (stop) {
+                    console.log("stop....");
+                break;
+            }
+            for (i = 0; i < arr.length; i++) {
+
+                if (stop) {
+                    console.log("stop....");
+                    break;
+                }
+
+                if (arr[i] > arr[i + 1]) {
+                    temp = arr[i];
+
+                    drawEvent.detail.px = i + 1;
+                    drawEvent.detail.height = arr[i + 1];
+                    drawEvent.detail.color = backColor;
+                    canvasRef.current?.dispatchEvent(drawEvent); 
+
+                    drawEvent.detail.px = i;
+                    drawEvent.detail.height = arr[i];
+                    drawEvent.detail.color = backColor;
+                    canvasRef.current?.dispatchEvent(drawEvent);
+
+                    arr[i] = arr[i + 1];
+
+                    drawEvent.detail.px = i;
+                    drawEvent.detail.height = arr[i];
+                    drawEvent.detail.color = foreColor;
+                    canvasRef.current?.dispatchEvent(drawEvent);
+
+                    arr[i + 1] = temp;
+                    drawEvent.detail.px = i + 1;
+                    drawEvent.detail.height = arr[i + 1];
+                    drawEvent.detail.color = foreColor;
+                    canvasRef.current?.dispatchEvent(drawEvent);
+
+                    await delay(delayTime);
+                }
+            }
+        }
+        console.log('done');
+        console.log(arr);
+    }
 
     return (
         <div className="flex justify-center items-center">
-            <button onClick={() => sort.bubbleSort(arr)} className="m-5">Start</button>
+            <button onClick={() => bubbleSort(arr)} className="m-5">Start</button>
             <button onClick={reset} className="m-5">Reset</button>
             <button onClick={clearBoard} className="m-5">Clear</button>
             <canvas ref={canvasRef} width={maxX} height={maxY} className="bg-black" />
