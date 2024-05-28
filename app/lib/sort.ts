687a972e45ecd1a45da09c017f930b3914b75c31
ShaikhMFazaluddin <fazaluddin2806@gmail.com>
2024-05-29 02:15:29 +0530

    working on merge sort

--- app/lib/sort.ts
+++ app/lib/sort.ts
@@ -8,21 +8,20 @@ export class sort {
     utils = new Utils;
     delayTime: number;
 
-
     constructor() {
         this.stopLoop = false;
         this.canvasRef = null;
         this.delayTime = 0;
     }
-    
-    async bubbleSort(arr: number[]) : Promise<void> {
+
+    async bubbleSort(arr: number[]): Promise<void> {
         const foreColor = "white", backColor = "black";
         let i: number, j, temp: number;
         let drawEvent = new CustomEvent("changeDraw", {
             detail: {
                 px: 0,
                 height: 0,
-                color: foreColor 
+                color: foreColor
             }
         });
 
@@ -43,7 +42,7 @@ export class sort {
                     drawEvent.detail.px = i + 1;
                     drawEvent.detail.height = arr[i + 1];
                     drawEvent.detail.color = backColor;
-                    this.canvasRef?.current?.dispatchEvent(drawEvent); 
+                    this.canvasRef?.current?.dispatchEvent(drawEvent);
 
                     drawEvent.detail.px = i;
                     drawEvent.detail.height = arr[i];
@@ -67,6 +66,37 @@ export class sort {
                 }
             }
         }
+    }
+
+
+    mergeSort(array: any[]): any[] {
+        if (array.length <= 1) {
+            return array;
+        }
+        const middle = Math.floor(array.length / 2);
+        const leftHalf = array.slice(0, middle);
+        const rightHalf = array.slice(middle);
+        return this.merge(this.mergeSort(leftHalf), this.mergeSort(rightHalf));
+    }
+
+    merge(left: any[], right: any[]): any[] {
+        let result: any[] = [];
+        let leftIndex = 0;
+        let rightIndex = 0;
+
+        while (leftIndex < left.length &&
+            rightIndex < right.length) {
+            if (left[leftIndex] < right[rightIndex]) {
+                result.push(left[leftIndex]);
+                leftIndex++;
+            } else {
+                result.push(right[rightIndex]);
+                rightIndex++;
+            }
+        }
+
+        return result.concat(left.slice(leftIndex)).
+            concat(right.slice(rightIndex));
     }
 
 }
--- app/ui/canvas.tsx
+++ app/ui/canvas.tsx
@@ -9,10 +9,10 @@ export default function Canvas() {
     const maxX = 400, maxY = 400;
     const lineWidth = 2;
     const sortO = new sort;
-    sortO.delayTime = 1;
+    sortO.delayTime = 0;
 
     let arr: number[] = [];
-    let max = 200;
+    let max = 150;
 
     const { canvasRef, clear } = useDraw(makeLine);
     sortO.canvasRef = canvasRef; 
@@ -33,22 +33,12 @@ export default function Canvas() {
         sortO.stopLoop = true;
     }
 
-    function initGraph() {
-
-        let initDraw = new CustomEvent(EventNames.DRAW_EVENT, {
-            detail: {
-                px: 0,
-                height: 0,
-            }
-        });
+    function initArray() {
         for (let i = 0; i < max; i++) {
             arr.push(Math.round(Math.random() * maxY));
-            initDraw.detail.px = i + 10;
-            initDraw.detail.height = arr[i];
-            canvasRef.current?.dispatchEvent(initDraw);
         }
     }
-    initGraph();
+    initArray();
 
     const reset = async () => {
         clear();
@@ -68,9 +58,15 @@ export default function Canvas() {
         }
     }
 
+    const merge = () => {
+        arr = sortO.mergeSort(arr);
+        console.log(arr);
+    }
+
     return (
         <div className="flex justify-center items-center">
-            <button onClick={() => sortO.bubbleSort(arr)} className="m-5">Start</button>
+            <button onClick={() => sortO.bubbleSort(arr)} className="m-5">Bubble sort</button>
+            <button onClick={merge} className="m-5">Merge sort</button>
             <button onClick={reset} className="m-5">Reset</button>
             <button onClick={clearBoard} className="m-5">Clear</button>
             <canvas ref={canvasRef} width={maxX} height={maxY} className="bg-black" />
