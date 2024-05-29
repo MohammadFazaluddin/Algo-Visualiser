fc6e75161043af11639a2f3e495f9f2d1adc8eba
ShaikhMFazaluddin <fazaluddin2806@gmail.com>
2024-05-29 17:00:54 +0530

    fixed merge sort, added shell sort

--- app/lib/sort.ts
+++ app/lib/sort.ts
@@ -1,5 +1,6 @@
 import { RefObject } from "react";
 import { Utils } from "./utils";
+import { EventNames } from "./event";
 
 export class sort {
 
@@ -7,21 +8,31 @@ export class sort {
     canvasRef: RefObject<HTMLCanvasElement> | null;
     utils = new Utils;
     delayTime: number;
+    maxY: number;
+    foreColor: string = "white";
+    backColor: string = "black";
 
     constructor() {
         this.stopLoop = false;
         this.canvasRef = null;
         this.delayTime = 0;
+        this.maxY = 0;
+    }
+
+    drawLine(x: number, height: number, color: string, event: CustomEvent): void {
+        event.detail.px = x;
+        event.detail.height = height;
+        event.detail.color = color;
+        this.canvasRef?.current?.dispatchEvent(event);
     }
 
     async bubbleSort(arr: number[]): Promise<void> {
-        const foreColor = "white", backColor = "black";
         let i: number, j, temp: number;
-        let drawEvent = new CustomEvent("changeDraw", {
+        let drawEvent = new CustomEvent(EventNames.DRAW_EVENT, {
             detail: {
                 px: 0,
                 height: 0,
-                color: foreColor
+                color: this.foreColor
             }
         });
 
@@ -39,28 +50,16 @@ export class sort {
                 if (arr[i] > arr[i + 1]) {
                     temp = arr[i];
 
-                    drawEvent.detail.px = i + 1;
-                    drawEvent.detail.height = arr[i + 1];
-                    drawEvent.detail.color = backColor;
-                    this.canvasRef?.current?.dispatchEvent(drawEvent);
+                    this.drawLine(i + 1, arr[i + 1], this.backColor, drawEvent);
 
-                    drawEvent.detail.px = i;
-                    drawEvent.detail.height = arr[i];
-                    drawEvent.detail.color = backColor;
-                    this.canvasRef?.current?.dispatchEvent(drawEvent);
+                    this.drawLine(i, arr[i], this.backColor, drawEvent);
 
                     arr[i] = arr[i + 1];
 
-                    drawEvent.detail.px = i;
-                    drawEvent.detail.height = arr[i];
-                    drawEvent.detail.color = foreColor;
-                    this.canvasRef?.current?.dispatchEvent(drawEvent);
+                    this.drawLine(i, arr[i], this.foreColor, drawEvent);
 
                     arr[i + 1] = temp;
-                    drawEvent.detail.px = i + 1;
-                    drawEvent.detail.height = arr[i + 1];
-                    drawEvent.detail.color = foreColor;
-                    this.canvasRef?.current?.dispatchEvent(drawEvent);
+                    this.drawLine(i + 1, arr[i + 1], this.foreColor, drawEvent);
 
                     await this.utils.delay(this.delayTime);
                 }
@@ -69,35 +68,150 @@ export class sort {
     }
 
 
-    mergeSort(array: any[]): any[] {
+    async mergeSort(array: any[], leftStart: number, rightStart: number): Promise<any[]> {
         if (array.length <= 1) {
             return array;
         }
+        if (this.stopLoop) {
+            return array;
+        }
+
+        const middleStart = Math.floor((leftStart + rightStart) / 2);
         const middle = Math.floor(array.length / 2);
         const leftHalf = array.slice(0, middle);
         const rightHalf = array.slice(middle);
-        return this.merge(this.mergeSort(leftHalf), this.mergeSort(rightHalf));
+        return await this.merge(await this.mergeSort(leftHalf, leftStart, middleStart),
+            await this.mergeSort(rightHalf, middleStart + 1, rightStart),
+            leftStart, middleStart + 1);
     }
 
-    merge(left: any[], right: any[]): any[] {
+    async merge(left: any[], right: any[], leftStart: number, rightStart: number): Promise<any[]> {
         let result: any[] = [];
         let leftIndex = 0;
         let rightIndex = 0;
 
-        while (leftIndex < left.length &&
-            rightIndex < right.length) {
-            if (left[leftIndex] < right[rightIndex]) {
-                result.push(left[leftIndex]);
-                leftIndex++;
-            } else {
-                result.push(right[rightIndex]);
-                rightIndex++;
-            }
+        if (this.stopLoop) {
+            return result;
         }
 
-        return result.concat(left.slice(leftIndex)).
-            concat(right.slice(rightIndex));
+
+        let drawEvent = new CustomEvent(EventNames.DRAW_EVENT, {
+            detail: {
+                px: 0,
+                height: 0,
+                color: this.foreColor
+            }
+        });
+
+        while (leftIndex < left.length &&
+            rightIndex < right.length) {
+
+            if (this.stopLoop) {
+                return result;
+            }
+            if (left[leftIndex] < right[rightIndex]) {
+                // erase previous drawing 
+
+                this.drawLine(leftStart, this.maxY, this.backColor, drawEvent);
+
+                this.drawLine(leftStart, left[leftIndex], this.foreColor, drawEvent);
+
+                result.push(left[leftIndex]);
+
+                leftIndex++;
+                leftStart++;
+            } else {
+
+                this.drawLine(rightStart, this.maxY, this.backColor, drawEvent);
+
+                this.drawLine(rightStart, right[rightIndex], this.foreColor, drawEvent);
+
+                result.push(right[rightIndex]);
+                rightIndex++;
+                rightStart++;
+            }
+            await this.utils.delay(this.delayTime);
+        }
+
+        while (leftIndex < left.length) {
+            if (this.stopLoop) {
+                return result;
+            }
+            this.drawLine(leftStart, this.maxY, this.backColor, drawEvent);
+
+            this.drawLine(leftStart, left[leftIndex], this.foreColor, drawEvent);
+
+            result.push(left[leftIndex++])
+            await this.utils.delay(this.delayTime);
+        }
+
+        while (rightIndex < right.length) {
+            if (this.stopLoop) {
+                return result;
+            }
+
+            this.drawLine(rightStart, this.maxY, this.backColor, drawEvent);
+
+            this.drawLine(rightStart, right[rightIndex], this.foreColor, drawEvent);
+
+            result.push(right[rightIndex++]);
+            rightStart++;
+            await this.utils.delay(this.delayTime);
+        }
+
+        for (let num = 0; num < result.length; num++) {
+            if (this.stopLoop) {
+                return result;
+            }
+
+            this.drawLine(num, this.maxY, this.backColor, drawEvent);
+
+            this.drawLine(num, result[num], this.foreColor, drawEvent);
+
+            await this.utils.delay(this.delayTime);
+        }
+
+        return result;
+    }
+
+    async shellSort(array: any[]) {
+        let gap: number, i: number, j: number, temp: number;
+
+        let drawEvent = new CustomEvent(EventNames.DRAW_EVENT, {
+            detail: {
+                px: 0,
+                height: 0,
+                color: this.foreColor
+            }
+        });
+ 
+        for (gap = Math.floor(array.length / 2); gap > 0; gap = Math.floor(gap / 2)) {
+            for (i = gap; i < array.length; i++) {
+                for (j = i - gap; j >= 0 && array[j] > array[j + gap]; j -= gap) {
+                    temp = array[j + gap];
+ 
+                    this.drawLine(j, array[j], this.backColor, drawEvent);
+ 
+                    this.drawLine(j + gap, array[j + gap], this.backColor, drawEvent);
+ 
+                    array[j + gap] = array[j];
+                    array[j] = temp;
+ 
+                    this.drawLine(j, array[j], this.foreColor, drawEvent);
+ 
+                    this.drawLine(j + gap, array[j + gap], this.foreColor, drawEvent);
+                    console.log(array[j]);
+ 
+                    await this.utils.delay(this.delayTime);
+
+                    if (this.stopLoop) {
+                        return array;
+                    }
+                }
+            }
+        } 
+        return array;
+
     }
 
 }
-
--- app/ui/canvas.tsx
+++ app/ui/canvas.tsx
@@ -3,19 +3,33 @@
 import { useDraw } from "./useDraw";
 import { EventNames } from "../lib/event";
 import { sort } from "../lib/sort";
+import { useEffect, useState } from "react";
 
 export default function Canvas() {
 
-    const maxX = 400, maxY = 400;
+    const maxX = 500, maxY = 500;
     const lineWidth = 2;
     const sortO = new sort;
-    sortO.delayTime = 0;
+    sortO.delayTime = 10;
+    sortO.maxY = maxY;
 
     let arr: number[] = [];
-    let max = 150;
+    let max = 100;
 
     const { canvasRef, clear } = useDraw(makeLine);
-    sortO.canvasRef = canvasRef; 
+    sortO.canvasRef = canvasRef;
+
+    const [inputDelay, setInputDelay] = useState(sortO.delayTime);
+
+    useEffect(() => {
+        sortO.delayTime = inputDelay;
+    }, [inputDelay]);
+
+    const [inputLength, setInputLength] = useState(max);
+
+    useEffect(() => {
+        max = inputLength;
+    }, [inputLength]);
 
     function makeLine({ ctx, px, height, color }: Draw) {
         const pos = px * lineWidth;
@@ -27,7 +41,7 @@ export default function Canvas() {
         ctx.stroke();
 
     }
-    
+
     const clearBoard = () => {
         clear();
         sortO.stopLoop = true;
@@ -51,26 +65,50 @@ export default function Canvas() {
             }
         });
         for (let i = 0; i < max; i++) {
-                arr[i] = (Math.round(Math.random() * 300));
-                initDraw.detail.px = i;
-                initDraw.detail.height = arr[i];
-                canvasRef.current?.dispatchEvent(initDraw);
+            arr[i] = (Math.round(Math.random() * 300));
+            initDraw.detail.px = i;
+            initDraw.detail.height = arr[i];
+            canvasRef.current?.dispatchEvent(initDraw);
         }
     }
 
-    const merge = () => {
-        arr = sortO.mergeSort(arr);
+    const merge = async () => {
+        arr = await sortO.mergeSort(arr, 0, arr.length);
+        console.log(arr);
+    }
+    const shell = async () => {
+        arr = await sortO.shellSort(arr);
         console.log(arr);
     }
 
     return (
-        <div className="flex justify-center items-center">
-            <button onClick={() => sortO.bubbleSort(arr)} className="m-5">Bubble sort</button>
-            <button onClick={merge} className="m-5">Merge sort</button>
-            <button onClick={reset} className="m-5">Reset</button>
-            <button onClick={clearBoard} className="m-5">Clear</button>
-            <canvas ref={canvasRef} width={maxX} height={maxY} className="bg-black" />
+        <div className="flex flex-col justify-center items-center p-5">
+            <div className="flex justify-center items-center">
+                <canvas ref={canvasRef} width={maxX} height={maxY} className="bg-black" />
+            </div>
+            <div className="flex flex-col gap-4 w-36 justify-center items-center">
+                <label>Delay: </label>
+                <input type="number" className="outline-black outline"
+                    value={inputDelay}
+                    onChange={(e) => setInputDelay(parseInt(e.target.value))} />
 
+                <label>Length: </label>
+                <input
+                    type="number"
+                    max={maxX}
+                    className="outline-black outline"
+                    value={inputLength} onChange={(e) => setInputLength(parseInt(e.target.value))} />
+            </div>
+            <div className="flex justify-center items-center">
+
+
+                <button onClick={() => sortO.bubbleSort(arr)} className="m-5">Bubble sort</button>
+                <button onClick={merge} className="m-5">Merge sort</button>
+                <button onClick={shell} className="m-5">Shell sort</button>
+                <button onClick={reset} className="m-5">Reset</button>
+                <button onClick={clearBoard} className="m-5">Clear</button>
+
+            </div>
         </div>
     );
 }
